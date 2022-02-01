6c324adf9ecbffb0d41d1df4c74a3f48bccb3f9c
shad0wflame <uriel.huertas@nivalis.io>
2022-02-01 23:36:28 +0100

    Added scss file and trunk tag.

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,47 @@ use yew::prelude::*;
 #[function_component(App)]
 fn app() -> Html {
     html! {
-        <h1>{ "Hello World" }</h1>
+         <>
+            <div class="container h-100">
+                <div class="row h-100 align-items-center justify-content-center">
+                    <div class="item-container">
+                        <div class="row align-content-center h-25 justify-content-center">
+                            <p class="title">
+                                {"Title"}
+                            </p>
+                        </div>
+                        <div class="row align-content-end h-50">
+                            <div id="class" class="col-4">
+                                {"Faction Item"}
+                            </div>
+                            <div id="faction" class="col-4">
+                                {"Race Item"}
+                            </div>
+                            <div id="race" class="col-4">
+                                {"Class Item"}
+                            </div>
+                        </div>
+                        <div class="row align-content-end h-25">
+                            <div class="col-4"/>
+                            <div class="col-4">
+                                <button type="button"
+                                        class="d-block mx-auto wow-btn">
+                                    {"Reset"}
+                                </button>
+                            </div>
+                            <div class="col-4">
+                                <button id="filter-btn" type="button"
+                                        class="d-block mx-auto wow-btn"
+                                        data-toggle="modal"
+                                        data-target="#filterModal">
+                                    {"Filters"}
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
     }
 }
 
