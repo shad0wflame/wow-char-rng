37f4f5ba5458b377a9852427a32938acef630bc1
shad0wflame <uriel.huertas@nivalis.io>
2022-02-02 20:00:35 +0100

    Use state in App component and pass data to Item components.

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,24 @@
-mod state;
 mod components;
+mod data;
+mod state;
 
+use crate::data::{get_random_class, get_random_icon, get_random_race, get_random_spec};
+use crate::state::{Action, State};
+use components::item::Item;
 use gloo::console::__macro::JsValue;
 use gloo_utils::body;
-use rand::{Rng, thread_rng};
+use rand::{thread_rng, Rng};
 use web_sys::CssStyleDeclaration;
 use yew::prelude::*;
-use components::item::Item;
 
 fn set_rand_background(style: &CssStyleDeclaration) -> Result<(), JsValue> {
     let mut rng = thread_rng();
     let image_index: u8 = rng.gen_range(0..=4);
 
-    style.set_property("background-image", &format!("url(assets/images/backgrounds/{}.jpg)", image_index))?;
+    style.set_property(
+        "background-image",
+        &format!("url(assets/images/backgrounds/{}.jpg)", image_index),
+    )?;
     style.set_property("background-size", "cover")?;
 
     Ok(())
@@ -20,11 +26,25 @@ fn set_rand_background(style: &CssStyleDeclaration) -> Result<(), JsValue> {
 
 #[function_component(App)]
 fn app() -> Html {
+    let state = use_reducer(State::default);
 
-    match set_rand_background(&body().style()) {
-        Err(e) => eprintln!("{:?}", e),
-        _ => ()
-    }
+    use_effect_with_deps(
+        move |_| {
+            match set_rand_background(&body().style()) {
+                Err(e) => eprintln!("{:?}", e),
+                _ => (),
+            }
+
+            // use_effect has to compulsorily return a closure, even if it's a no-op.
+            || {}
+        },
+        ()
+    );
+
+    let reset_onclick = {
+        let state = state.clone();
+        Callback::from(move |_| state.dispatch(Action::Reset))
+    };
 
     html! {
         <div class="container h-100">
@@ -37,24 +57,45 @@ fn app() -> Html {
                     </div>
                     <div class="row align-content-end h-50">
                         <div id="class" class="col-4">
-                            <Item text={"Horde"} icon={"2px"} class_name={"faction horde"} />
+                            <Item
+                                text={state.selected_race.faction.clone()}
+                                icon={"2px"}
+                                class_name={format!(
+                                    "faction {}",
+                                    &state.selected_race.faction.to_lowercase()
+                                )}
+                            />
                         </div>
                         <div id="faction" class="col-4">
-                            <Item text={"Dark Iron Dwarf"} icon={"-485px -160px"} class_name={"race"} />
+                            <Item
+                                text={state.selected_race.name.clone()}
+                                icon={get_random_icon(&state.selected_race)}
+                                class_name={"race"}
+                            />
                         </div>
                         <div id="race" class="col-4">
-                            <Item text={"Brewmaster Monk"} icon={"0px -75px"} class_name={"class"} />
+                            <Item
+                                text={format!(
+                                    "{} {}",
+                                    get_random_spec(&state.selected_class),
+                                    &state.selected_class.name
+                                )}
+                                icon={state.selected_class.icon.clone()}
+                                class_name={"class"}
+                            />
                         </div>
                     </div>
                     <div class="row align-content-end h-25">
                         <div class="offset-md-4 col-4">
                             <button type="button"
                                     class="d-block mx-auto wow-btn"
+                                    onclick={reset_onclick}
                             >
                                 {"Repeat!"}
                             </button>
                         </div>
                         <div class="col-4">
+                            /*
                             <button id="filter-btn"
                                     type="button"
                                     class="d-block mx-auto wow-btn"
@@ -63,6 +104,7 @@ fn app() -> Html {
                             >
                                 {"Filters"}
                             </button>
+                            */
                         </div>
                     </div>
                 </div>
